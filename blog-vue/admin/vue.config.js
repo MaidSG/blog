bf98a849b4b017ce1d9f8ac1962d3f3674154f28
Joe <ifinetoo@qq.com>
2023-02-20 09:32:50 +0800

    add new

--- blog-vue/admin/vue.config.js
+++ blog-vue/admin/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://43.136.176.80:8080",
         changeOrigin: true,
         pathRewrite: {
           "^/api": ""
